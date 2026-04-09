0c2c64611ede6e567970a852b3e8d12a24f15638
Exploring-glitch <deysasha262@gmail.com>
2026-04-09 21:41:34 +0530

    feat: add user model with schema validation

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
-const userModel = new mongoose.Schema({
-    name:{type: String, unique: true, }
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String, 
+        unique: [true,"Username already taken"], 
+        required: true, 
+    },
+    email: {
+        type: String,
+        unique: [true,"Account already exists with this email address"], 
+        required: true, 
+    },
+    password: {
+        type: String,
+        required: true, 
+    }
 })
 
 
+const userModel = mongoose.model("users", userSchema)
+module.exports = userModel
